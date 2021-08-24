666732bbc3a036bd683b615bd195289621b96a2f
martinuslee <supreme034@korea.ac.kr>
2021-08-24 17:15:26 +0900

    gps fixed

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 import os
+import torch
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from PIL import Image
-from PIL.ExifTags import TAGS
+from PIL.ExifTags import TAGS,GPSTAGS
 
 app = Flask(__name__)
+
+
+#RESULT_FOLDER = os.path.join('static/')
+#app.config['RESULT_FOLDER'] = RESULT_FOLDER
+
+#model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).autoshape()  # for PIL/cv2/np inputs and NMS
+#model.eval()
+
+def get_prediction(img_bytes):
+    img = Image.open(io.BytesIO(img_bytes))
+    imgs = [img]  # batched list of images
+
+    # Inference
+    results = model(imgs, size=640)  # includes NMS
+    return results
+
 
 @app.route('/upload')
 def render_file():
@@ -13,9 +30,11 @@ def render_file():
 @app.route('/fileUpload', methods = ['POST'])
 def upload_file():
     if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
         f = request.files['file']
         #저장할 경로 + 파일명
-        #f.save('/Users/jongheon/dev/electricscooter/static/uploads/' + secure_filename(f.filename))
+        f.save('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
         #f = request.files['file1'].read()
         image = Image.open(request.files['file'].stream)
         info = image._getexif();
@@ -25,8 +44,9 @@ def upload_file():
         
         for tag, value in info.items():
             decoded = TAGS.get(tag, tag)
+            
             taglabel[decoded] = value
-
+        print(taglabel)
         exifGPS = taglabel['GPSInfo']
         latData = exifGPS[2]
         lonData = exifGPS[4]
@@ -74,4 +94,4 @@ def index():
 '''
 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
+    app.run(host='0.0.0.0', port='5550', debug=True)
