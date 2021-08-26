3a4ed59dc2620ac07388ee5745ee4ca16bb42bf6
martinuslee <supreme034@korea.ac.kr>
2021-08-26 11:00:33 +0900

    flask map position add

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ from werkzeug.utils import secure_filename
 from PIL import Image
 from PIL.ExifTags import TAGS,GPSTAGS
 from GPSPhoto import gpsphoto
+import ssl
+import copy
 
 app = Flask(__name__)
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='v5s_e300_best.pt')  # default
 model.eval()
+
+msg = '주차하셔도 좋은 장소 입니다.'
+# nohup python3 -u app.py &
+#tail -f nohup.out
+#종료 lsof -i :5550
 
 def get_prediction(img_bytes):
     # Inference
@@ -20,23 +27,37 @@ def get_prediction(img_bytes):
     results.save(PATH)
     return results
 
-
+object_list = list()
 
 def results_img(results):
+    object_list.clear()
+    global msg
     print(results.names)
     pred = results.pred
     for i in pred[0]:
     #print(int(i[-1]))
     #print(results.names[int(i[-1])])
         objects = results.names[int(i[-1])]
-        print(objects)
-        if(objects != 'electric_scooter'):
-            msg = '주차하시면안되용!'
-        else:
-            msg = '주차하셔도 좋은 장소 입니다.'
-    
-    return msg
+        object_list.append(objects)
+        #detected = object_list
+        #detected = copy.deepcopy(object_list)
         
+        # if(objects != 'electric_scooter'):
+        #     msg = '주차하시면안되용!'
+        # else:
+        #     msg = '주차하셔도 좋은 장소 입니다.'
+        # return msg
+
+    if(any('electric_scooter' in i for i in object_list) != True):
+        return '킥보드가 검출되지 않았습니다. 재촬영 해주세요!'
+
+    output = any('brailleblock' in i or 'crosswalk' in i or 'fireplug' in i for i in object_list)
+    print(output)
+
+    if output:
+        return "주차 불가 구역입니다."
+    else:
+        return "주차 가능한 장소입니다."
   
 
 #print(results.names[int(results.pred[0][0][-1])])
@@ -47,12 +68,14 @@ def render_file():
 
 @app.route('/fileUpload', methods = ['GET','POST'])
 def upload_file():
+    global check_msg
     if request.method == 'POST':
         if 'file' not in request.files:
             return redirect(request.url)
         f = request.files['file']
         #저장할 경로 + 파일명
         f.save('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
+        print(secure_filename(f.filename))
         #f = request.files['file1'].read()
         image = Image.open(request.files['file'].stream)
         #img_bytes = file.read()
@@ -60,14 +83,15 @@ def upload_file():
         
         #image_names = os.listdir(img_path)
         
-        data = gpsphoto.getGPSData('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
-        print(data)
+        #data = gpsphoto.getGPSData('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
+        #print(data)
         #print(data['Latitude'], data['Longitude'])
-        geocode = data['Latitude'], data['Longitude']
+        #geocode = data['Latitude'], data['Longitude']
         #img_path = '/home/dkjh/datacampus/electricscooter/runs/detect/exp/image0.jpg'
 
         check_msg = results_img(results)
-    return render_template('index.html', geocode = geocode, check = check_msg)
+    #return render_template('index.html', geocode = geocode, check = check_msg)
+    return render_template('index.html', check = check_msg, obj = object_list)
 
 '''
 @app.route('/map')
@@ -76,4 +100,6 @@ def index():
 '''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5550', debug=True)
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/usr/local/ssl/server.crt', keyfile='/usr/local/ssl/server.key', password='bluesea34')
+    app.run(host='0.0.0.0', port='5550', debug=True, ssl_context=ssl_context)
--- test.py
+++ test.py
@@ -1,45 +1,94 @@
-#!/usr/bin/env python
-
+import io
+import os
+import requests
+import torch
+from flask import Flask, jsonify, url_for, render_template, request, redirect
+from werkzeug.utils import secure_filename
 from PIL import Image
-from PIL.ExifTags import TAGS
-from PIL.ExifTags import GPSTAGS
+from PIL.ExifTags import TAGS,GPSTAGS
+from GPSPhoto import gpsphoto
+import ssl
 
-def get_exif(filename):
-    image = Image.open(filename)
-    image.verify()
-    return image._getexif()
+app = Flask(__name__)
 
-exif = get_exif('/home/dkjh/datacampus/electricscooter/static/photos/KakaoTalk_20210820_160455003_01.jpg')
-print(exif)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='v5s_e300_best.pt')  # default
+model.eval()
 
-def get_labeled_exif(exif):
-    labeled = {}
-    for (key, val) in exif.items():
-        labeled[TAGS.get(key)] = val
+def get_prediction(img_bytes):
+    # Inference
+    results = model(img_bytes, size=640)  # includes NMS
+    PATH = './static/'
+    results.save(PATH)
+    return results
 
-    return labeled
+object_list = list()
+def results_img(results):
+    global msg
+    print(results.names)
+    pred = results.pred
+    for i in pred[0]:
+    #print(int(i[-1]))
+    #print(results.names[int(i[-1])])
+        objects = results.names[int(i[-1])]
+        object_list.append(objects)
+        
+        # if(objects != 'electric_scooter'):
+        #     msg = '주차하시면안되용!'
+        # else:
+        #     msg = '주차하셔도 좋은 장소 입니다.'
+        # return msg
+    print(object_list)
 
-exif = get_exif('/home/dkjh/datacampus/electricscooter/static/photos/KakaoTalk_20210820_160455003_01.jpg')
-labeled = get_labeled_exif(exif)
-print(labeled)
+    if(any('electric_scooter' in i for i in object_list) != True):
+        return '킥보드가 검출되지 않았습니다. 재촬영 해주세요!'
 
+    output = any('brailleblock' in i or 'crosswalk' in i or 'fireplug' in i for i in object_list)
+    print(output)
 
-def get_geotagging(exif):
-    if not exif:
-        raise ValueError("No EXIF metadata found")
+    if output:
+        return "주차 불가 구역입니다."
+    else:
+        return "주차 가능한 장소입니다."
 
-    geotagging = {}
-    for (idx, tag) in TAGS.items():
-        if tag == 'GPSInfo':
-            if idx not in exif:
-                raise ValueError("No EXIF geotagging found")
+#print(results.names[int(results.pred[0][0][-1])])
 
-            for (key, val) in GPSTAGS.items():
-                if key in exif[idx]:
-                    geotagging[val] = exif[idx][key]
+@app.route('/upload')
+def render_file():
+    return render_template('upload.html')
 
-    return geotagging
+@app.route('/fileUpload', methods = ['GET','POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        f = request.files['file']
+        #저장할 경로 + 파일명
+        f.save('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
+        print(secure_filename(f.filename))
+        #f = request.files['file1'].read()
+        image = Image.open(request.files['file'].stream)
+        #img_bytes = file.read()
+        results = get_prediction(image)
+        
+        #image_names = os.listdir(img_path)
+        
+        #data = gpsphoto.getGPSData('/home/dkjh/datacampus/electricscooter/static/photos/' + secure_filename(f.filename))
+        #print(data)
+        #print(data['Latitude'], data['Longitude'])
+        #geocode = data['Latitude'], data['Longitude']
+        #img_path = '/home/dkjh/datacampus/electricscooter/runs/detect/exp/image0.jpg'
 
-exif = get_exif('/home/dkjh/datacampus/electricscooter/static/photos/KakaoTalk_20210820_160455003_01.jpg')
-geotags = get_geotagging(exif)
-print(geotags)
+        check_msg = results_img(results)
+    #return render_template('index.html', geocode = geocode, check = check_msg)
+    return render_template('index.html', check = check_msg)
+
+'''
+@app.route('/map')
+def index():
+    return render_template('index.html')
+'''
+
+if __name__ == '__main__':
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+    ssl_context.load_cert_chain(certfile='/usr/local/ssl/server.crt', keyfile='/usr/local/ssl/server.key', password='bluesea34')
+    app.run(host='0.0.0.0', port='5550', debug=True, ssl_context=ssl_context)
